5cab99a3e7d03d5611d82bdcbd65b54ac106cc98
Naveen Kumar M N <NaveenKumarMN20@users.noreply.github.com>
2020-08-29 11:41:34 +0530

    Code Alligned and removed unwanted imports

--- src/main/java/io/tofts/s3/controlerinterfaces/S3ControllerInterface.java
+++ src/main/java/io/tofts/s3/controlerinterfaces/S3ControllerInterface.java
@@ -1,14 +1,9 @@
 package io.tofts.s3.controlerinterfaces;
 
+import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
 import software.amazon.awssdk.services.s3.model.DeleteBucketResponse;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
-
-import org.springframework.http.ResponseEntity;
 
 
 public interface S3ControllerInterface {
@@ -17,9 +12,8 @@ public interface S3ControllerInterface {
 
     Mono<DeleteBucketResponse> DeleteBucket(String bucketName);
 
-    Mono<ResponseEntity> fileDownload (String fileName, String buckeName);
+    Mono<ResponseEntity> fileDownload(String fileName, String buckeName);
 
-
-	Mono<ResponseEntity> uploadHandler(String fileName, String bucketName);
+    Mono<ResponseEntity> uploadHandler(String fileName, String bucketName);
 
 }
